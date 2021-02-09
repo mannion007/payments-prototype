32ee1f8adab00a56b78cb5999c695560f7d35929
James Mannion <james.mannion@pay4later.com>
2021-02-09 10:18:49 +0000

    replace hard coded publisher with http publisher

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/ThreeDotsLabs/watermill v1.1.1
 	github.com/ThreeDotsLabs/watermill-amqp v1.1.0
+	github.com/ThreeDotsLabs/watermill-http v1.1.3
 	github.com/gogo/protobuf v1.2.1
 	github.com/golang/protobuf v1.4.3
 	github.com/google/uuid v1.2.0
--- main.go
+++ main.go
@@ -2,19 +2,22 @@ package main
 
 import (
 	"context"
+	"io/ioutil"
 	"log"
+	stdHttp "net/http"
 	"time"
 
 	"github.com/ThreeDotsLabs/watermill"
 	"github.com/ThreeDotsLabs/watermill-amqp/pkg/amqp"
+	"github.com/ThreeDotsLabs/watermill-http/pkg/http"
 	"github.com/ThreeDotsLabs/watermill/message"
 	"github.com/ThreeDotsLabs/watermill/message/router/middleware"
 	"github.com/ThreeDotsLabs/watermill/message/router/plugin"
-	"github.com/golang/protobuf/proto"
 	"github.com/google/uuid"
 	"github.com/mannion007/payments-prototype/pkg/handler"
 	"github.com/mannion007/payments-prototype/pkg/payment"
 	"github.com/mannion007/payments-prototype/pkg/processor"
+	"google.golang.org/protobuf/proto"
 )
 
 const (
@@ -25,7 +28,8 @@ const (
 )
 
 var (
-	logger = watermill.NewStdLogger(false, false)
+	logger   = watermill.NewStdLogger(false, false)
+	httpAddr = ":8888"
 )
 
 func main() {
@@ -38,12 +42,36 @@ func main() {
 
 	amqpConfig := amqp.NewDurableQueueConfig(amqpURI)
 
-	// configure subscriber
+	// configure http subscriber (takes http request ad publishes message to bus)
+	httpSubscriber, err := http.NewSubscriber(
+		httpAddr,
+		http.SubscriberConfig{
+			UnmarshalMessageFunc: func(topic string, request *stdHttp.Request) (*message.Message, error) {
+				b, err := ioutil.ReadAll(request.Body)
+				if err != nil {
+					return nil, err
+				}
+
+				return message.NewMessage(watermill.NewUUID(), b), nil
+			},
+		},
+		logger,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// configure message subscriber (takes message from bus and processes it)
 	subscriber, err := amqp.NewSubscriber(amqpConfig, logger)
 	if err != nil {
 		panic(err)
 	}
 	defer subscriber.Close()
+
+	if err != nil {
+		panic(err)
+	}
 
 	// [DEBUG] print all the events produced
 	router.AddNoPublisherHandler(
@@ -71,15 +99,45 @@ func main() {
 			Multiplier:      2.0,
 		}.Middleware, // retry upto 10 times with a backoff duration 1s, 2s, 4s, 8s...
 		middleware.Recoverer, // recovers from panics in handlers, enabling the message to be retried
-		middleware.NewThrottle(100, time.Second).Middleware, // slow down the processing to 10 messages per second
+		middleware.NewThrottle(1000, time.Second).Middleware, // slow down the processing to 10 messages per second
 	)
 
 	// instantiate the handler
 	processor := processor.NewStripeProcessor()
-	takePaymentHandler := handler.NewClaimPayment(processor)
+	claimPaymentHandler := handler.NewClaimPayment(processor)
 
-	// start publishing messages
-	//go publishMessages(publisher)
+	// add a handler for converting web requests to commands
+	router.AddHandler(
+		"http_to_bus",
+		"/pay",
+		httpSubscriber,
+		commandTopic,
+		publisher,
+		func(msg *message.Message) ([]*message.Message, error) {
+			// todo - make this interpret the web request and build the command from it (this is all just hard coded)
+			payee := uuid.New()
+
+			claim := &payment.Claim{
+				Payee:  payee.String(),
+				Amount: &payment.Claim_MonetaryAmount{Currency: "GBP", Value: 10000},
+				Payer: &payment.Claim_Card{
+					Number:    "13656978",
+					ExpiresAt: &payment.Claim_ExpirationDate{Year: 2025, Month: 12},
+				},
+			}
+
+			buf, err := proto.Marshal(claim)
+			if err != nil {
+				panic(err)
+			}
+
+			m := message.NewMessage(watermill.NewUUID(), buf)
+			middleware.SetCorrelationID(watermill.NewUUID(), msg)
+
+			return []*message.Message{m}, nil
+
+		},
+	)
 
 	// add a handler for taking payments to the router
 	router.AddHandler(
@@ -88,45 +146,18 @@ func main() {
 		subscriber,
 		eventTopic,
 		publisher,
-		takePaymentHandler.Process,
+		claimPaymentHandler.Process,
 	)
+
+	go func() {
+		// wait until the router is running then start the webserver in another go routine
+		<-router.Running()
+		_ = httpSubscriber.StartHTTPServer()
+	}()
 
 	// Run the router
 	if err := router.Run(context.Background()); err != nil {
 		panic(err)
-	}
-}
-
-// publishMessages loops forever puttiing commands onto the command bus
-func publishMessages(publisher message.Publisher) {
-
-	payee := uuid.New()
-
-	claim := &payment.Claim{
-		Payee:  payee.String(),
-		Amount: &payment.Claim_MonetaryAmount{Currency: "GBP", Value: 10000},
-		Payer: &payment.Claim_Card{
-			Number:    "13656978",
-			ExpiresAt: &payment.Claim_ExpirationDate{Year: 2025, Month: 12},
-		},
-	}
-
-	buf, err := proto.Marshal(claim)
-	if err != nil {
-		panic(err)
-	}
-
-	for {
-		//time.Sleep(time.Duration(time.Second)) // throttle the messages for development
-
-		msg := message.NewMessage(watermill.NewUUID(), buf)
-		middleware.SetCorrelationID(watermill.NewUUID(), msg)
-
-		log.Printf("sending message %s, correlation id: %s\n", msg.UUID, middleware.MessageCorrelationID(msg))
-
-		if err := publisher.Publish(commandTopic, msg); err != nil {
-			panic(err)
-		}
 	}
 }
 
